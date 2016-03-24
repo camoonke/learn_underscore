d71c94f619b03a51b38bdf8f360829fb167653cf
camoonke <nhjjzcj@gmail.com>
2016-03-24 20:44:44 +0800

    _.first, _.initial,_.last,_.rest,_.compact

--- test/lib/array.test.js
+++ test/lib/array.test.js
@@ -1,13 +1,28 @@
 var _ = require('../../');
 
 describe('test/array.test.js', function () {
-  describe('Arrays', function () {
-    describe('#chunk', function () {
-      it('_.chunk(arr, 2)', function () {
-        // var arr = ['a', 'b', 'c', 'd', 'e', 'f']
-        // _.chunk(arr, 2).should.eql([['a', 'b'], ['c', 'd'], ['e', 'f']])
-      })
-    })
+  it('#first', function () {
+    _.first([5, 4, 3, 2, 1]).should.eql(5)
+    _.first([5, 4, 3, 2, 1], 2).should.eql([5, 4])
   })
-  
+
+  it('#initial', function () {
+    _.initial([5, 4, 3, 2, 1]).should.eql([5, 4, 3, 2])
+    _.initial([5, 4, 3, 2, 1], 2).should.eql([5, 4, 3])
+  })
+
+  it('#last', function () {
+    _.last([5, 4, 3, 2, 1]).should.eql(1)
+    _.last([5, 4, 3, 2, 1], 2).should.eql([2, 1])
+  })
+
+  it('#rest', function () {
+    _.rest([5, 4, 3, 2, 1]).should.eql([4, 3, 2, 1])
+    _.rest([5, 4, 3, 2, 1], 2).should.eql([3, 2, 1])
+  })
+
+  it('#compact', function () {
+    _.compact([0, 1, false, 2, '', 3]).should.eql([1, 2, 3])
+  })
+
 })
